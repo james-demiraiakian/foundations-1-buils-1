6da0c786defc050dd4f7f66adba2197ec70126b4
James Demiraiakian <james.armen@gmail.com>
2021-10-08 13:37:16 -0700

    randome number generates, tests pass

--- app.js
+++ app.js
@@ -7,3 +7,11 @@
 // use user input to update state 
 // update DOM to reflect the new state
 
+const generate = document.getElementById('generate');
+const randomNum = document.getElementById('random-num');
+
+
+
+generate.addEventListener('click', () => {
+    randomNum.textContent = Math.floor(Math.random() * 2147483647);
+});
